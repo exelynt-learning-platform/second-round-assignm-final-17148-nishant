5bd23f991d4114e48df363df5ed846764122b58c
patilnishant9194 <156905830+patilnishant9194@users.noreply.github.com>
2026-04-02 14:45:43 +0530

    fourth commit

--- src/main/java/com/nishant/ecommerce/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.nishant.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nishant.ecommerce.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
